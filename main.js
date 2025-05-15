b753911b5a926d0fed936f64939facca0b14e8e9
rawrwho <villamayor.em@obmontessori.edu.ph>
2025-05-15 09:43:08 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function displayProductCategory() {
+	var categoryEntry = document.getElementById('categoryEntry').value;
+
+	switch(categoryEntry){
+	case "C":
+		document.getElementById('displayCategory').innerHTML = "Category: Clothing";
+		break;
+
+	case "E":
+		document.getElementById('displayCategory').innerHTML = "Category: Electronics";
+		break;
+
+	case "B":
+		document.getElementById('displayCategory').innerHTML = "Category: Books";
+		break;
+
+	default:
+		alert("Invalid Input");
+	}
+}
